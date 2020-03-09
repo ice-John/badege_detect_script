ac5885f3de03597ae439704c9793fa50979d9e33
Jared Wenerd <wenerd87@gmail.com>
2020-03-09 14:24:11 -0400

    Various cleanup, formatting, fixes for production; Add model file; Modify example.

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 # coding=utf-8
-
-
-import pandas as pd 
+import pandas as pd
 import pickle
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
@@ -26,22 +24,21 @@ import requests
 import json
 import sys
 
+SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+
 stop_words_en = set(stopwords.words('english'))
 
 
-def get_num_words(badge_response):
-    text_to_process = badge_response
-
+def get_num_words(text_to_process):
     tokens = word_tokenize(text_to_process)
-    num_words = len(tokens)
-
-    return str(num_words)
+    return int(len(tokens))
 
 
 def tokens(sent):
     return nltk.word_tokenize(sent)
 
-def getNumSpellErrors(response):
+
+def num_spelling_errors(response):
     num_errors = 0
 
     for i in tokens(response):
@@ -59,49 +56,54 @@ def getNumSpellErrors(response):
     return num_errors
 
 
+GRAMMAR_API_ENDPOINT = 'https://virtualwritingtutor.com/API/checkgrammar.php'
+GRAMMAR_API_KEY = 'ab4d4823-8b99-11e8-a062-00163e747eab'
+
+
 def get_grammar(badge_response):
-    api_endpoint = 'https://virtualwritingtutor.com/API/checkgrammar.php'
-    api_key = 'ab4d4823-8b99-11e8-a062-00163e747eab'
-    api_text = badge_response
+    try:
+        r = requests.post(url=GRAMMAR_API_ENDPOINT, data={
+                          'appKey': GRAMMAR_API_KEY, 'text': badge_response})
 
-    api_data = {
-            'appKey':api_key,
-            'text':api_text
-            }
+        data = json.loads(r.text)
+        if data['status'] == '401':
+            raise Exception(data['message'])
+        if 'error_grammar_percent' in data:
+            return data['error_grammar_percent']
+        else:
+            raise Exception(r.text)
+    except Exception as e:
+        return {'error': repr(e)}
+    else:
+        return False
 
-    r = requests.post(url = api_endpoint, data = api_data)
-    data = json.loads(r.text)
 
+def get_scores(text):
+    tokenizer = pickle.load(open(SCRIPT_PATH + '/tokenizer.p', 'rb'))
+    model = load_model(SCRIPT_PATH + '/model0.h5')
 
-    error_pct = data['error_grammar_percent']
+    X_tokens = tokenizer.texts_to_sequences([text])
+    X_pad = pad_sequences(X_tokens, maxlen=100, padding='post')
+    result_ls = list(model.predict_proba(X_pad))[0]
 
-    return error_pct
-
-
+    score_likeliness = {'1': 0, '2': 0, '3': 0, '4': 0,
+                        '5': 0, '6': 0, '7': 0, '8': 0, '9': 0, '10': 0}
+    i = 0
+    for k in score_likeliness.keys():
+        score_likeliness[k] = float(format(result_ls[i] * 100, '.2f'))
+        i = i + 1
+    return score_likeliness
 
 
 def main():
-    tokenizer = pickle.load(open('tokenizer.p','rb'))
-    model = load_model('model0.h5')
-    message_input = sys.argv[1:]
-    X_tokens = tokenizer.texts_to_sequences([message_input])
-    X_pad = pad_sequences(X_tokens, maxlen = 100, padding = 'post')
-    #X_pad = X_pad.astype(np.float64)
-    result_ls = list(model.predict_proba(X_pad))[0]
-    #result_ls = result_ls.astype(np.float64)
-    num_words = get_num_words(message)
-    NumSpellErrors = getNumSpellErrors(message)
-    grammar = get_grammar(message)
-    result = {'one':0,'two':0,'three':0,'four':0,'five':0,'six':0,'seven':0,'eight':0,'nine':0,'ten':0}
-    i=0
-    for k in result.keys():
-    	result[k] = result_ls[i]
-    	i=i+1
-    result['num_word']=num_words
-    result['NumSpellErrors']=NumSpellErrors
-    result['grammar']=grammar
-    print(result)
-
+    text_input = sys.argv[1]
+    result = {
+        'scores': get_scores(text_input),
+        'num_word': get_num_words(text_input),
+        'num_spelling_errors': num_spelling_errors(text_input),
+        'grammar': get_grammar(text_input)
+    }
+    print(json.dumps(result))
 
 
 if __name__ == '__main__':
